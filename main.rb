1d693bb99a94478cf38ff3c7bce8ebbf75c580e6
Catalin <catalinboruga@yahoo.com>
2014-08-05 13:37:01 +0200

    My first init

---
+++ main.rb
@@ -0,0 +1,26 @@
+require 'sass'
+require 'sinatra'
+require 'rubygems'
+require 'sinatra/reloader' if development?
+
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  erb :home
+end
+
+get '/about' do 
+	@title = "All About This Website"
+	erb :about
+end
+
+get '/contact' do
+  erb :contact
+end
+
+not_found do 
+	erb :not_found
+end
+
+
+
